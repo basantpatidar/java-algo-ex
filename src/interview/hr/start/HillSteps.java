a81a05b24b75358267a649f4e942a862c2672071
basantpatidar <basantpatidar33@gmail.com>
2020-05-16 21:55:05 -0400

    Hill Step count

---
+++ src/interview/hr/start/HillSteps.java
@@ -0,0 +1,38 @@
+package interview.hr.start;
+
+import java.io.*;
+
+public class HillSteps {
+	
+    // Complete the countingValleys function below.
+    static int countingValleys(int n, String s) {
+    	int hill = 0;
+    	int valley = 0;
+    	for(int i = 0; i < s.length(); i++) {
+    		if(s.charAt(i) == 'U') {
+    			++hill;
+    			System.out.println("U => " + hill );
+    		}else {
+    			--hill;
+    			System.out.println("D => " + hill);
+    		}
+    		if(hill == 0 && s.charAt(i) == 'U') {
+    			valley++;
+    		}
+    		
+    	}
+    	System.out.println(valley);
+    	return valley;
+    }
+
+
+    public static void main(String[] args) throws IOException {
+      
+        String s = "UDDDUDUUUUDUUDDDDDUDDUUU";
+
+//        int result = 
+        		HillSteps.countingValleys(8, s);
+
+        
+    }
+}
