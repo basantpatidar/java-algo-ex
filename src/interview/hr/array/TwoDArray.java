1449561805541784dbd554243fa3bd4757d361f0
Basant Patidar <34193893+basantpatidar@users.noreply.github.com>
2020-05-22 23:00:36 -0400

    Update TwoDArray.java

--- src/interview/hr/array/TwoDArray.java
+++ src/interview/hr/array/TwoDArray.java
@@ -21,9 +21,6 @@ public class TwoDArray {
     
     public static void main(String[] args){
 
-//        int[][] arr = new int[6][6];
-
-//        int result = 
         int[][] arr = {{-9,-9,-9,1,1,1},{0,-9,0,4,3,2},{-9,-9,-9,1,2,3},{0,0,8,6,6,0},{0,0,0,-2,0,0},{0,0,1,2,4,0}};
         		hourglassSum(arr);
 
