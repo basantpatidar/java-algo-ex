022a2d6d03f5c8122f784341cf3180a4b4c57f79
basantpatidar <basantpatidar33@gmail.com>
2020-05-27 22:20:15 -0400

    update

---
+++ src/interview/hr/array/BribeQueue.java
@@ -0,0 +1,20 @@
+package interview.hr.array;
+
+public class BribeQueue {
+
+    // Complete the minimumBribes function below.
+    static void minimumBribes(int[] q) {
+    	
+    	for(int i = 0; i < q.length; i++) {
+    		}
+
+    }
+
+
+    public static void main(String[] args) {
+    		
+    	int[] q = {2, 1, 5, 3, 4};
+    	BribeQueue.minimumBribes(q);
+
+   }
+}
