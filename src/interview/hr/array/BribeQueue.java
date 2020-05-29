ec72bc63d06b76fdfae608e3c5cde9d0cf450df9
basantpatidar <basantpatidar33@gmail.com>
2020-05-28 20:05:04 -0400

    fix

--- src/interview/hr/array/BribeQueue.java
+++ src/interview/hr/array/BribeQueue.java
@@ -5,9 +5,28 @@ public class BribeQueue {
     // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
     	
-    	for(int i = 0; i < q.length; i++) {
-    		}
-
+    	for(int i=0;i<q.length;i++){
+            if((q[i] - (i+1)) > 2){
+                System.out.println("Too chaotic");
+                return; 
+            }
+        }
+               
+        //now we just need to count number of Swaps
+        int swaps=0;
+        for(int i=0;i< q.length;i++){
+            for(int j=i+1;j<q.length;j++){
+                if(q[i] > q[j]){ 
+                    int tmp=q[j];
+                    q[j]=q[i];
+                    q[i]=tmp;
+                    swaps++;
+                }
+            }
+        }
+        
+        System.out.println(swaps);
+    
     }
 
 
