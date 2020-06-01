2e96c66e181daf5372dd902d4aa3d9e0a0e44071
basantpatidar <basantpatidar33@gmail.com>
2020-05-31 21:10:53 -0400

    update

--- src/interview/algo/sort/BubbleSort.java
+++ src/interview/algo/sort/BubbleSort.java
@@ -9,21 +9,9 @@ public class BubbleSort {
 	    }
 
 	    public static void main(String[] args) {
-	        int n = scanner.nextInt();
-	        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-	        int[] a = new int[n];
-
-	        String[] aItems = scanner.nextLine().split(" ");
-	        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-	        for (int i = 0; i < n; i++) {
-	            int aItem = Integer.parseInt(aItems[i]);
-	            a[i] = aItem;
-	        }
-
-	        countSwaps(a);
-
-	        scanner.close();
+	    	
+	    	int[] a = {1,4,2,5,3,6};
+	    	
+	    	BubbleSort.countSwaps(a);
 	    }
 	}
