13a7647e274ae5f8e82ffed3b18bc7ecfea3513f
basantpatidar <basantpatidar33@gmail.com>
2020-06-01 21:14:47 -0400

    update

--- src/interview/algo/sort/BubbleSort.java
+++ src/interview/algo/sort/BubbleSort.java
@@ -5,7 +5,18 @@ public class BubbleSort {
 	    // Complete the countSwaps function below.
 	    static void countSwaps(int[] a) {
 
-
+	    	for (int i = 0; i < n; i++) {
+	    	    
+	    	    for (int j = 0; j < n - 1; j++) {
+	    	        // Swap adjacent elements if they are in decreasing order
+	    	        if (a[j] > a[j + 1]) {
+	    	            swap(a[j], a[j + 1]);
+	    	        }
+	    	    }
+	    	}
+	    }
+	    public int[] swap(int a, int a2) {
+	    	return 0;
 	    }
 
 	    public static void main(String[] args) {
